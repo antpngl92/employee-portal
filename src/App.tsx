2ff4fb36d27b424c3f470f16b6d2b6d4dcb0da4b
Anton Nyagolov <a.nyagolov@se18.qmul.ac.uk>
2025-07-08 00:08:32 +0300

    Create Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from "react-router"
 import { Navbar } from "./components"
+import { Home } from "./pages"
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="container mx-auto px-4 pt-24 flex-grow">
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </main>
     </div>
   )
