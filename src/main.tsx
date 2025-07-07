90832efbc340fa7086601a73fe6a1e8a00243cb9
Anton Nyagolov <a.nyagolov@se18.qmul.ac.uk>
2025-07-08 00:22:38 +0300

    Wrap App with EmployeesProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router'
+import { EmployeesProvider } from './context/employee'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <EmployeesProvider>
       <App />
+    </EmployeesProvider>
     </BrowserRouter>
   </StrictMode>,
 )
