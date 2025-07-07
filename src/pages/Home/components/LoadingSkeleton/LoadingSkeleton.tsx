be82ccc077ef0f6069dc5f43b8db9e0aed6fc144
Anton Nyagolov <a.nyagolov@se18.qmul.ac.uk>
2025-07-08 01:50:35 +0300

    Lint Home page LoadingSkeleton

--- src/pages/Home/components/LoadingSkeleton/LoadingSkeleton.tsx
+++ src/pages/Home/components/LoadingSkeleton/LoadingSkeleton.tsx
@@ -1,31 +1,31 @@
-import { 
-  Card, 
-  CardHeader, 
-  CardContent, 
-  CardFooter, 
-  Skeleton 
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Skeleton
 } from "@/components";
 
 const EmployeeCardSkeleton = () => {
   return (
-<Card data-testid="employee-card-skeleton" className="flex flex-col border rounded-lg overflow-hidden animate-pulse">
-    <CardHeader className="flex items-center space-x-4 p-4">
-      <Skeleton className="h-12 w-12 rounded-full" />
+    <Card data-testid="employee-card-skeleton" className="flex flex-col border rounded-lg overflow-hidden animate-pulse">
+      <CardHeader className="flex items-center space-x-4 p-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
 
-      <div className="flex-1 space-y-2">
-        <Skeleton className="h-4 w-24 rounded-md" />  
-        <Skeleton className="h-3 w-16 rounded-md" />
-      </div>
-    </CardHeader>
+        <div className="flex-1 space-y-2">
+          <Skeleton className="h-4 w-24 rounded-md" />
+          <Skeleton className="h-3 w-16 rounded-md" />
+        </div>
+      </CardHeader>
 
-    <CardContent className="flex-1 px-4">
-      <Skeleton className="h-4 w-3/4 rounded-md" />
-    </CardContent>
+      <CardContent className="flex-1 px-4">
+        <Skeleton className="h-4 w-3/4 rounded-md" />
+      </CardContent>
 
-    <CardFooter className="flex justify-end p-4">
-      <Skeleton className="h-8 w-20 rounded-md" />
-    </CardFooter>
-  </Card>
+      <CardFooter className="flex justify-end p-4">
+        <Skeleton className="h-8 w-20 rounded-md" />
+      </CardFooter>
+    </Card>
   );
 }
 
