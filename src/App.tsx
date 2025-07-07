c8565d9798ceb16bddb66a92f6adde6270d6f6f4
Anton Nyagolov <a.nyagolov@se18.qmul.ac.uk>
2025-07-08 00:04:16 +0300

    Setup a Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Navbar } from "./components"
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <Navbar />
       <main className="container mx-auto px-4 pt-24 flex-grow">
-        
+
       </main>
     </div>
   )
